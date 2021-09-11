2d2fb9817c7166b6720235fc5cfb986271c17bc7
EduardF1 <fischer_eduard@yahoo.com>
2021-09-11 20:34:04 +0200

    refactor api paths and add confirmation prompt

--- src/actions/projectActions.ts
+++ src/actions/projectActions.ts
@@ -15,7 +15,7 @@ export const createProject = (project: any, history: any) => async (dispatch: an
 }
 
 export const getProjects = () => async (dispatch: any) => {
-    const res = await axios.get("http://localhost:8080/api/project/all");
+    const res = await axios.get("/api/project/all");
     dispatch({
         type: GET_PROJECTS,
         payload: res.data
@@ -24,20 +24,22 @@ export const getProjects = () => async (dispatch: any) => {
 
 export const getProject = (id: string, history: any) => async (dispatch: any) => {
     try {
-        const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+        const res = await axios.get(`/api/project/${id}`);
         dispatch({
             type: GET_PROJECT,
             payload: res.data
         })
-    } catch (err:any) {
+    } catch (err: any) {
         history.push("/dashboard");
     }
 }
 
-export const deleteProject = (id:string) => async (dispatch:any) => {
-    await axios.delete(`http://localhost:8080/api/project/${id}`);
-    dispatch({
-        type:DELETE_PROJECT,
-        payload:id
-    })
+export const deleteProject = (id: string) => async (dispatch: any) => {
+    if (window.confirm("Are you sure ? This will delete the project and all of its related data.")) {
+        await axios.delete(`/api/project/${id}`);
+        dispatch({
+            type: DELETE_PROJECT,
+            payload: id
+        })
+    }
 }
