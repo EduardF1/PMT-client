7715002a3003707b93ef5d088a28515230c3ec3e
EduardF1 <fischer_eduard@yahoo.com>
2021-09-12 16:16:36 +0200

    refactor the Backlog component

--- src/components/project-board/project-tasks/ProjectTask.tsx
+++ src/components/project-board/project-tasks/ProjectTask.tsx
@@ -1,54 +1,28 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 class ProjectTask extends React.Component<any, any> {
 
     render() {
-        const {id} = this.props.match.params;
 
         return (
-            <div className="add-PBI">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <Link to={`/projectBoard/${id}`} className="btn btn-light">
-                                Back to Project Board
-                            </Link>
-                            <h4 className="display-4 text-center">Add Project Task</h4>
-                            <p className="lead text-center">Project Name + Project Code</p>
-                            <form>
-                                <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary"/>
-                                </div>
-                                <div className="form-group">
-                                    <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
-                                </div>
-                                <h6>Due Date</h6>
-                                <div className="form-group">
-                                    <input type="date" className="form-control form-control-lg" name="dueDate"/>
-                                </div>
-                                <div className="form-group">
-                                    <select className="form-control form-control-lg" name="priority">
-                                        <option value={0}>Select Priority</option>
-                                        <option value={1}>High</option>
-                                        <option value={2}>Medium</option>
-                                        <option value={3}>Low</option>
-                                    </select>
-                                </div>
+            <div className="card mb-1 bg-light">
 
-                                <div className="form-group">
-                                    <select className="form-control form-control-lg" name="status">
-                                        <option value="">Select Status</option>
-                                        <option value="TO_DO">TO DO</option>
-                                        <option value="IN_PROGRESS">IN PROGRESS</option>
-                                        <option value="DONE">DONE</option>
-                                    </select>
-                                </div>
+                <div className="card-header text-primary">
+                    ID: projectSequence -- Priority: priorityString
+                </div>
+                <div className="card-body bg-light">
+                    <h5 className="card-title">project_task.summary</h5>
+                    <p className="card-text text-truncate ">
+                        project_task.acceptanceCriteria
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                        View / Update
+                    </a>
 
-                                <input type="submit" className="btn btn-primary btn-block mt-4"/>
-                            </form>
-                        </div>
-                    </div>
+                    <button className="btn btn-danger ml-4">
+                        Delete
+                    </button>
                 </div>
             </div>
         );
