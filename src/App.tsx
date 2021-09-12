7aab4aa9c81a56c271631853e2fd59c85aaf9b63
EduardF1 <fischer_eduard@yahoo.com>
2021-09-12 12:26:19 +0200

    did small clean-up, change proxy address, initial component setup (avoid TS errors)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
-import Dashboard from "./components/Dashboard";
-import Header from "./components/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import AddProject from "./components/project/AddProject";
-import {Provider} from "react-redux";
-import store from "./store";
+import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProject from './components/project/AddProject';
+import {Provider} from 'react-redux';
+import store from './store';
 import UpdateProject from './components/project/UpdateProject';
+import ProjectBoard from './components/project-board/ProjectBoard';
 
 function App() {
     return (
         <Provider store={store}>
             <Router>
-                <div className="App">
+                <div className='App'>
                     <Header/>
-                    <Route exact path="/dashboard" component={Dashboard}/>
-                    <Route exact path="/addProject" component={AddProject}/>
-                    <Route exact path="/updateProject/:id" component={UpdateProject}/>
+                    <Route exact path='/dashboard' component={Dashboard}/>
+                    <Route exact path='/addProject' component={AddProject}/>
+                    <Route exact path='/updateProject/:id' component={UpdateProject}/>
+                    <Route exact path='/projectBoard/:id' component={ProjectBoard}/>
                 </div>
             </Router>
         </Provider>
---
+++ src/components/project-board/ProjectBoard.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ProjectBoard extends Component {
+    render() {
+        return (
+            <h1>Welcome to Project Board</h1>
+        );
+    }
+}
+
+export default ProjectBoard;
