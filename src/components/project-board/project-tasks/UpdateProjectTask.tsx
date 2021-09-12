27bd0ef2c760e2cc17033da95bcee66767df3d89
EduardF1 <fischer_eduard@yahoo.com>
2021-09-12 22:00:01 +0200

    add lifecycle hooks for UpdateProjectTask component

--- src/components/project-board/project-tasks/UpdateProjectTask.tsx
+++ src/components/project-board/project-tasks/UpdateProjectTask.tsx
@@ -8,9 +8,74 @@ class UpdateProjectTask extends React.Component<any, any> {
 
     public static propTypes = {};
 
+    // initial component state
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            id: "",
+            projectSequence: "",
+            summary: "",
+            acceptanceCriteria: "",
+            status: "",
+            priority: "",
+            dueDate: "",
+            projectIdentifier: "",
+            createdAt: ""
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
     componentDidMount() {
         const {backlogId, projectTaskId} = this.props.match.params;
         this.props.getProjectTask(backlogId, projectTaskId, this.props.history);
+    }
+
+    // state after component mount
+    componentWillReceiveProps(nextProps: any) {
+        const {
+            id,
+            projectSequence,
+            summary,
+            acceptanceCriteria,
+            status,
+            priority,
+            dueDate,
+            projectIdentifier,
+            createdAt
+        } = nextProps.projectTask;
+
+        this.setState({
+            id,
+            projectSequence,
+            summary,
+            acceptanceCriteria,
+            status,
+            priority,
+            dueDate,
+            projectIdentifier,
+            createdAt
+        });
+    }
+
+    onChange(event: any) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    onSubmit(event:any){
+        event.preventDefault();
+        const updatedProjectTask = {
+            id:this.state.id,
+            projectSequence:this.state.projectSequence,
+            summary:this.state.summary,
+            acceptanceCriteria:this.state.acceptanceCriteria,
+            status:this.state.status,
+            priority:this.state.priority,
+            dueDate:this.state.dueDate,
+            projectIdentifier:this.state.projectIdentifier,
+            createdAt:this.state.createdAt
+        };
+        console.log(updatedProjectTask);
     }
 
     render() {
@@ -23,20 +88,43 @@ class UpdateProjectTask extends React.Component<any, any> {
                                 Back to Project Board
                             </a>
                             <h4 className="display-4 text-center">Update Project Task</h4>
-                            <p className="lead text-center">Project Name + Project Code</p>
+                            <p className="lead text-center">Project Name: {this.state.projectIdentifier}|Project Task ID: {this.state.projectSequence}</p>
                             <form>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary"/>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        name="summary"
+                                        placeholder="Project Task summary"
+                                        value={this.state.summary}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                                 <div className="form-group">
-                                    <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                                    <textarea
+                                        className="form-control form-control-lg"
+                                        placeholder="Acceptance Criteria"
+                                        name="acceptanceCriteria"
+                                        value={this.state.acceptanceCriteria ? this.state.acceptanceCriteria : ""}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                                 <h6>Due Date</h6>
                                 <div className="form-group">
-                                    <input type="date" className="form-control form-control-lg" name="dueDate"/>
+                                    <input
+                                        type="date"
+                                        className="form-control form-control-lg"
+                                        name="dueDate"
+                                        value={this.state.dueDate ? this.state.dueDate : ""}
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                                 <div className="form-group">
-                                    <select className="form-control form-control-lg" name="priority">
+                                    <select className="form-control form-control-lg"
+                                            name="priority"
+                                            value={this.state.priority}
+                                            onChange={this.onChange}
+                                    >
                                         <option value={0}>Select Priority</option>
                                         <option value={1}>High</option>
                                         <option value={2}>Medium</option>
@@ -45,7 +133,12 @@ class UpdateProjectTask extends React.Component<any, any> {
                                 </div>
 
                                 <div className="form-group">
-                                    <select className="form-control form-control-lg" name="status">
+                                    <select
+                                        className="form-control form-control-lg"
+                                        name="status"
+                                        value={this.state.status}
+                                        onChange={this.onChange}
+                                    >
                                         <option value="">Select Status</option>
                                         <option value="TO_DO">TO DO</option>
                                         <option value="IN_PROGRESS">IN PROGRESS</option>
